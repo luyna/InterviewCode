7d114176db530affbec67caccb47ad88e0f184cb
luyna <yuluan1991@gmail.com>
2015-01-27 16:59:22 +0800

    添加冒泡排序和归并排序

--- src/base/Sort.java
+++ src/base/Sort.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 /**
  * 各种排序算法
  * 1、冒泡排序  O(n) O(n^2) 空间O(1) 
- * 2、归并排序  
+ * 2、归并排序  O(nlgn) 空间O(n)
  * 
  * 测试用例：
  * []
  * [2]
  * [6,3]
  * [9,6,4,3,5]
+ * [10,4,8,9]
  * @author luyna
  * 2015年1月27日
  */
@@ -63,6 +64,59 @@ public class Sort {
 		}
 		return arr;
 	}
+	/**
+	 * 2、归并排序  O(nlgn) 空间O(n)
+	 * @param arr
+	 * @return
+	 */
+	public int[] mergeSort(int[] arr){
+		if(arr.length==0 || arr.length==1) return arr;
+		int len=arr.length;
+		mergeSortHelper(arr,0,len-1);
+		return arr;
+	}
+	/**
+	 * 辅助函数：递归实现数组的分解和并
+	 * @param arr
+	 * @param p 数组开始下标
+	 * @param q 数组结束下标
+	 */
+	public void mergeSortHelper(int[]arr,int p,int q){
+		if(p>=q) return;
+		int m=p+(p-q)>>1;    //二分分解，能用移位的尽量用移位运算
+		mergeSortHelper(arr,p,m);
+		mergeSortHelper(arr,m+1,q);
+		merge(arr,p,m,q);
+	}
+	/**
+	 * 将数组的两个字数组合并
+	 * @param arr
+	 * @param p 第一个子数组开始下标
+	 * @param q 第一个子数组结束下标
+	 * @param r 第二个子数组结束下标
+	 */
+	public void merge(int[] arr,int p,int q,int r){
+		int i=p,j=q+1;
+		int [] tmp=new int [r-p+1];
+		int count=0;
+		while(i<=q && j<=r){
+			if(arr[i]<=arr[j]){
+				tmp[count++]=arr[i++];
+			}else tmp[count++]=arr[j++];
+		}
+		while(i<=q){
+			tmp[count++]=arr[i++];
+		}
+		while(j<=r){
+			tmp[count++]=arr[j++];
+		}
+		//将排好序的临时数组拷贝回原始数组
+		while(count>0){
+			arr[r--]=tmp[count-1];
+			count--;
+		}
+		
+	}
 	
 	
 	
@@ -72,12 +126,20 @@ public class Sort {
 		int [] test2={2};
 		int [] test3={6,3};
 		int [] test4={9,6,4,3,5};
+		int [] test5={10,4,8,9};
 		Sort sort=new Sort();
-		System.out.println("————————bubbleSort test————————");
+		System.out.println("————————1 bubbleSort test————————");
 		System.out.println("test1"+Arrays.toString(sort.bubbleSort(test1)));
 		System.out.println("test2"+Arrays.toString(sort.bubbleSort(test2)));
 		System.out.println("test3"+Arrays.toString(sort.bubbleSort(test3)));
 		System.out.println("test4"+Arrays.toString(sort.bubbleSort(test4)));
+		System.out.println("test5"+Arrays.toString(sort.bubbleSort(test5)));
+		System.out.println("————————2 mergeSort test————————");
+		System.out.println("test1"+Arrays.toString(sort.mergeSort(test1)));
+		System.out.println("test2"+Arrays.toString(sort.mergeSort(test2)));
+		System.out.println("test3"+Arrays.toString(sort.mergeSort(test3)));
+		System.out.println("test4"+Arrays.toString(sort.mergeSort(test4)));
+		System.out.println("test5"+Arrays.toString(sort.mergeSort(test5)));
 	}
 
 }
