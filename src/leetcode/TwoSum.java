47dd4f801e3eb9f8d02f0e9a5ead1ba7c0ac2197
luyna <yuluan1991@gmail.com>
2015-01-28 15:10:15 +0800

    两数之和解法二

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -20,7 +20,13 @@ import java.util.HashMap;
 	Output: index1=1, index2=2
  */
 public class TwoSum {
-	 public int[] twoSum(int[] numbers, int target) {
+	/**
+	 * 方法一：用hash表保存数组中的数值，并判断target-number[i]是否存在于哈希表中
+	 * @param numbers
+	 * @param target
+	 * @return
+	 */
+	 /*public int[] twoSum(int[] numbers, int target) {
 	     HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
 	     int tmp1=0,tmp2=0;
 	     for(int i=0;i<numbers.length;i++){
@@ -35,5 +41,26 @@ public class TwoSum {
 	     }
   		 if(tmp1>=tmp2) return new int[]{tmp2,tmp1};
 		 else return new int[]{tmp1,tmp2};
+	 }*/
+	 /**
+	  * 思路二：如果数组是有序的，从左至右依次递增（该题没有此条件，如用该方法需要先排序）
+	  * 用两个指针i,j分别从数组两端开始分别指向当前相加的两个数值，如果两数和等于target，则返回；
+	  * 如果两数和小于target，则左边指针右移
+	  * 如果两数只和大于target，则右边指针左移
+	  * 
+	  * @param numbers
+	  * @param target
+	  * @return
+	  */
+	 public int[] twoSum(int[] numbers, int target) {
+		 int i=0,j=numbers.length-1;
+		 if(numbers.length<=1) return new int[]{-1,-1}; 
+		 while(i<j){
+			 if(numbers[i]+numbers[j]==target) return new int[]{i+1,j+1};
+			 else if(numbers[i]+numbers[j]<target) i++;
+			 else j--;
+		 }
+		 return new int[]{-1,-1};
 	 }
+	 
 }
